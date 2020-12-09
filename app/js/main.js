07dc5eb83827cb364e788501bd643f5eef9bee7f
serrgio06 <serrgio06@yandex.ru>
2020-12-09 23:25:23 +0500

    added features and products section

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,8 @@
 $(function(){
 
-
+  $('.slider__week').slick({
+    prevArrow: '<button class="slick-arrow slick-prev"></button>',
+    nextArrow: '<button class="slick-arrow slick-next"></button>',
+  });
 
 });
