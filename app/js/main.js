d93600ea13055f77943c7546225b7c90511ed316
serrgio06 <serrgio06@yandex.ru>
2020-12-18 23:16:26 +0500

    added slider product

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,18 @@ $(function(){
   });
   var mixer = mixitup('.new__products-inner');
 
+  $('.followers__slider-wrap').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    variableWidth: false,
+    prevArrow: '<button class="slick-arrow slick-prev"></button>',
+    nextArrow: '<button class="slick-arrow slick-next"></button>',
+  });
+
+  $(".box__options-star").rateYo({
+    rating: 4,
+    starWidth: "12px",
+    readOnly: true
+  });
+
 });
